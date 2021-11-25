a5d750af33bd225ee928634d1baa9600b74966d9
Elmer Huitz <elmohuitz@gmail.com>
2021-11-25 10:46:41 -0600

    changed plot size

--- js/mapper.js
+++ js/mapper.js
@@ -1,34 +1,66 @@
-// DATA
-const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
-
-const routes = [
-    ['PHX', 'LAX'],
-    ['PHX', 'JFK'],
-    ['JFK', 'OKC'],
-    ['JFK', 'HEL'],
-    ['JFK', 'LOS'],
-    ['MEX', 'LAX'],
-    ['MEX', 'BKK'],
-    ['MEX', 'LIM'],
-    ['MEX', 'EZE'],
-    ['LIM', 'BKK'],
-];
+// CMPS3252 - Algorithms Project 
+// Elmer R Huitz 2014110553
 
 
-// The graph
-const adjacencyList = new Map();
+//Scatter Chart Constructor
+var chart = new CanvasJS.Chart("chartContainer", {
+	zoomEnabled: true,
+	zoomType: "xy",
+	title: {
+		text: "Chart from Text File"
+	},
+	axisX: {
+		title: "X",
+		maximum: 11,
+		interval: 1,
+		gridColor: "lightblue",
+		gridThickness: 1
+	},
+	axisY: {
+		title: "Y",
+		maximum: 11,
+		interval: 1,
+		gridColor: "lightblue",
+		gridThickness: 1
+	},
+	data: [
+		{
+			type: "scatter",
+			toolTipContent: "<b>X: </b>{x}<br/><b>Y: </b>{y}",
+			dataPoints: [],
+			markerSize: 30,
+		}
+	]
+});
 
-// Add node
-function addNode(airport) {
-    adjacencyList.set(airport, []);
+//-- Event handler for the input tag
+var inputElement = document.getElementById("listOfPointsInput");
+inputElement.addEventListener("change", handleFiles, false);
+
+function handleFiles() {
+	var fileList = this.files;
+	var reader = new FileReader();
+
+	setInterval(function () {
+		reader.readAsText(fileList[0]);
+		reader.onload = function () {
+			renderChart(reader);
+		}
+	}, 1000);
 }
 
-// Add edge, undirected
-function addEdge(origin, destination) {
-    adjacencyList.get(origin).push(destination);
-    adjacencyList.get(destination).push(origin);
-}
+function renderChart(reader) {
+	var strDps = reader.result;
+	var dps = [];
 
-// Create the Graph
-airports.forEach(addNode);
-routes.forEach(route => addEdge(...route))
+	strDps = strDps.split("\n");
+
+	for (var i = 0; i < strDps.length; i++) {
+		dps.push({
+			x: parseInt(strDps[i].split(",")[0]),
+			y: parseInt(strDps[i].split(",")[1])
+		});
+	}
+	chart.options.data[0].dataPoints = dps;
+	chart.render();
+}

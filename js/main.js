be4a0216362c28a388d2392b277d035354286961
Elmer Huitz <elmohuitz@gmail.com>
2021-11-25 08:50:19 -0600

    canvasjs attempt. .

--- js/main.js
+++ js/main.js
@@ -10,11 +10,13 @@
       .forEach(function (form) {
         form.addEventListener('submit', function (event) {
           if (!form.checkValidity()) {
+              
             event.preventDefault()
             event.stopPropagation()
+           
           }
   
-          form.classList.add('was-validated')
+          form.classList.add('was-validated')         
         }, false)
       })
   })()
